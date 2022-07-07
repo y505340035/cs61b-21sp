bb57db9acda68614f4efe9f7a3a61248cd4e7318
y505340035 <505340035@qq.com>
2022-07-07 19:16:43 +0800

    lab2

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -16,6 +16,14 @@ public class DebugExercise2 {
         return max;
     }
 
+    public static int reviseMax(int a, int b){
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+
 
     /** Returns the sum of a and b. Do not step into this function. */
     public static int add(int a, int b) {
@@ -46,7 +54,7 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
+            int biggerValue = reviseMax(a[i], b[i]);
             returnArray[i] = biggerValue;
         }
 
@@ -58,7 +66,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = sum + x[i];
             i = i + 1;
         }
         return sum;
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,28 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple2(){
+        IntList lst = IntList.of(2, 4, 6, 8, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 6 -> 8 -> 10", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesSimple3(){
+        IntList lst = IntList.of(12, 4, 6, 8, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("12 -> 4 -> 6 -> 8 -> 10", lst.toString());
+        assertTrue(!changed);
+    }
+
+    @Test
+    public void testSquarePrimesSimple4(){
+        IntList lst = IntList.of(2, 2, 2, 2, 2);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 4 -> 4 -> 4 -> 4", lst.toString());
+        assertTrue(changed);
+    }
 }
