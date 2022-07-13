2b505748dcc011e7adc49ea5faa929fd52249aed
y505340035 <505340035@qq.com>
2022-07-13 18:16:34 +0800

    projEC done

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -11,6 +11,7 @@ public class TestArrayDequeEC {
     public void randomizedTest() {
         StudentArrayDeque<Integer> studentArrayDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> arrayDequeSolution = new ArrayDequeSolution<>();
+        String wrongMsg = new String("\n");
 
         int N = 500000;
         for (int i = 0; i < N; i++) {
@@ -19,30 +20,37 @@ public class TestArrayDequeEC {
                 //addFirst
                 studentArrayDeque.addFirst(i);
                 arrayDequeSolution.addFirst(i);
+                wrongMsg += "addFirst" + "(" + i + ")\n";
             } else if (randomNum == 1  && !studentArrayDeque.isEmpty() && !arrayDequeSolution.isEmpty()) {
                 //removeFirst
                 Integer s = studentArrayDeque.removeFirst();
                 Integer a = arrayDequeSolution.removeFirst();
-                assertEquals(s, a);
+                wrongMsg += "removeFirst()\n";
+                assertEquals(wrongMsg, s, a);
             } else if (randomNum == 2) {
                 //size
-                assertEquals(studentArrayDeque.size(), arrayDequeSolution.size());
+                wrongMsg += "size()\n";
+                assertEquals(wrongMsg, studentArrayDeque.size(), arrayDequeSolution.size());
             } else if (randomNum == 3) {
                 //isEmpty
-                assertEquals(studentArrayDeque.isEmpty(), arrayDequeSolution.isEmpty());
+                wrongMsg += "isEmpty()\n";
+                assertEquals(wrongMsg, studentArrayDeque.isEmpty(), arrayDequeSolution.isEmpty());
             } else if (randomNum == 4) {
                 //addLast
                 studentArrayDeque.addLast(i);
                 arrayDequeSolution.addLast(i);
+                wrongMsg += "addLast" + "(" + i + ")\n";
             } else if (randomNum == 5 && !studentArrayDeque.isEmpty() && !arrayDequeSolution.isEmpty()) {
                 //removeLast
                 Integer s = studentArrayDeque.removeLast();
                 Integer a = arrayDequeSolution.removeLast();
-                assertEquals(s, a);
+                wrongMsg += "removeLast()\n";
+                assertEquals(wrongMsg, s, a);
             } else if (randomNum == 6 && !studentArrayDeque.isEmpty() && !arrayDequeSolution.isEmpty()) {
                 //get
                 int randomGet = StdRandom.uniform(0, studentArrayDeque.size());
-                assertEquals(studentArrayDeque.get(randomGet), arrayDequeSolution.get(randomGet));
+                wrongMsg += "get" + "(" + randomGet + ")\n";
+                assertEquals(wrongMsg, studentArrayDeque.get(randomGet), arrayDequeSolution.get(randomGet));
             }
         }
     }
